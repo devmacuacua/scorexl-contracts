31026c11e1d8b7ffbff3a52e0958072b9db762e6
Francisco Macuacua <macuacuafrancisco@gmail.com>
2026-05-22 21:19:58 +0200

    Initial Project

---
+++ src/test/java/com/scorexl/contracts/ScorexlContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorexl.contracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorexlContractsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
